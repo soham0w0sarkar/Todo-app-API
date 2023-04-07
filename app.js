9df0c72b7c33329dcaafe7d351fb4e944a1ab011
soham0w0sarkar <sohamsarkar59@gmail.com>
2023-04-08 00:27:51 +0530

    adding cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import taskRouter from "./routes/task.js";
 import { errorMidlleWare } from "./utils/error.js";
+import cors from "cors";
 
 export const app = express();
 
@@ -14,6 +15,13 @@ config({
 //addinf midllewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/task", taskRouter);
 app.use(errorMidlleWare);
